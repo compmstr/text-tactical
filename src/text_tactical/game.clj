ded390389ba27a7da894effa4ab7c331cc55ebc1
Corey <corey@undiwork>
2013-05-29 10:25:18 -0400

    Refactored to have window be global in text-tactical.window ns

--- src/text_tactical/game.clj
+++ src/text_tactical/game.clj
@@ -1,8 +1,9 @@
 (ns text-tactical.game
+  (use [text-tactical.window :only [window]])
   (require [swing-text.ui :as ui]
            [swing-text.events :as events]
            [swing-text.menu :as menu]
-           [text-tactical.menu-data :as menu-data]))
+           [text-tactical.main-menu :as main-menu]))
 
 (defn exit-game
   []
@@ -10,41 +11,26 @@
   ;;(System/exit 0)
   )
 
-(defn- main-do-start
-  [win]
-  ;;(events/pop-keymap! win)
+(defn- on-main-start
+  []
+  ;;(events/pop-keymap! @window)
   (println "Starting game"))
-(defn- main-do-settings
-  [win]
-  ;;(events/pop-keymap! win)
+(defn- on-main-settings
+  []
+  ;;(events/pop-keymap! @window)
   (println "Settings"))
-(defn- do-exit
-  [win]
-  (events/reset-keymap-stack! win)
+(defn- on-main-exit
+  []
+  (events/reset-keymap-stack! @window)
   (println "Exiting...")
   (exit-game))
+(def main-callbacks
+  {:start on-main-start 
+   :settings on-main-settings
+   :exit on-main-exit})
 
-(defn- main-menu-callback
-  [win main-menu]
-  (menu/create-menu-callback
-   {:before (fn [] 
-              (.clear! win)
-              (main-menu :draw win)
-              (.repaint win))
-    :activate {:start (partial main-do-start win)
-               :settings (partial main-do-settings win)
-               :exit (partial do-exit win)}
-    :cancel (fn []
-              (println "Main Menu cancel")
-              (do-exit win))}))
-
-(def main-menu (apply menu/create-menu menu-data/main-menu-data))
 
 (defn game-start
-  [win]
-  (.clear! win)
+  []
   ;;(main-menu :bind win (main-menu-callback win main-menu))
-  (main-menu :push-bindings win (main-menu-callback win main-menu))
-  (main-menu :draw win)
-  (.repaint win))
-  
+  (main-menu/show-main-menu main-callbacks on-main-exit))
